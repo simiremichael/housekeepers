ada9fa562c59e40ba467c3f7144f49f52dc6a851
Michael Simire <simiremichael@gmail.com>
2024-05-01 19:53:52 +0100

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
     <main className="">
       <Navbar />
       <TopContainer />
+      <ServiceContainer />
       <MiddleContainer />
       <ServiceContainer />
       <WhyContainer />
